48bb7af3144bc9a1e8cec33dddf35e879e53a36f
robertSahm <robertsahm@gmail.com>
2019-04-18 16:22:55 -0600

    checking font performance live

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,91 +1,118 @@
 var proxy = require("http-proxy-middleware")
 
 module.exports = {
-  siteMetadata: {
-    title: "Robby Sahm's Portfolio",
-    description: "The Portfolio for Robby Sahm's web development",
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: false, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-  // for avoiding CORS while developing Netlify Functions locally
-  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
-    app.use(
-      "/.netlify/functions/",
-      proxy({
-        target: "http://localhost:9000",
-        pathRewrite: {
-          "/.netlify/functions/": "",
-        },
-      })
-    )
-  },
+	siteMetadata: {
+		title: "Robby Sahm's Portfolio",
+		description: "The Portfolio for Robby Sahm's web development"
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sass",
+		{
+			// keep as first gatsby-source-filesystem plugin for gatsby image support
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/static/img`,
+				name: "uploads"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: "pages"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/img`,
+				name: "images"
+			}
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					{
+						resolve: "gatsby-remark-relative-images",
+						options: {
+							name: "uploads"
+						}
+					},
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth: 2048
+						}
+					},
+					{
+						resolve: "gatsby-remark-copy-linked-files",
+						options: {
+							destinationDir: "static"
+						}
+					}
+				]
+			}
+		},
+		{
+			resolve: "gatsby-plugin-netlify-cms",
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`
+			}
+		},
+		{
+			resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+			options: {
+				develop: false, // Activates purging in npm run develop
+				purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
+			}
+		}, // must be after other CSS plugins
+		{
+			resolve: `gatsby-plugin-prefetch-google-fonts`,
+			options: {
+				fonts: [
+					{
+						family: `EB Garamond`,
+						variants: [
+							`400`,
+							`400i`,
+							`500`,
+							`500i`,
+							`600`,
+							`600i`,
+							`700`,
+							`700i`,
+							`800`,
+							`800i`
+						],
+						subsets: [`latin`]
+					},
+					{
+						family: `Open Sans`,
+						variants: [`400`, `700`]
+					}
+				]
+			}
+		},
+		"gatsby-plugin-netlify" // make sure to keep it last in the array
+	],
+	// for avoiding CORS while developing Netlify Functions locally
+	// read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+	developMiddleware: app => {
+		app.use(
+			"/.netlify/functions/",
+			proxy({
+				target: "http://localhost:9000",
+				pathRewrite: {
+					"/.netlify/functions/": ""
+				}
+			})
+		)
+	}
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,25 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-// import Features from '../components/Features'
-// import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
   image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
 }) => (
   <div>
 
     <section className="section">
-      <Link to="about">heyyyy</Link>
-
+      {/* <Link to="about">heyyyy</Link> */}
+      <div className="content">
+        <h1 className="title is-1">Robby Sahm</h1>
+      </div>
     </section>
   </div>
 )
@@ -31,23 +23,9 @@ const IndexPage = ({ data }) => {
     <Layout>
       <IndexPageTemplate
         image={frontmatter.image}
-        // title={frontmatter.title}
-        // heading={frontmatter.heading}
-        // subheading={frontmatter.subheading}
-        // mainpitch={frontmatter.mainpitch}
-        // description={frontmatter.description}
-        // intro={frontmatter.intro}
       />
     </Layout>
   )
-}
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
 }
 
 export default IndexPage
